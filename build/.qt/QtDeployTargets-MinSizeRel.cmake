514e394bc89f9a75ef5816e917b3f62e41388b1b
aquila21 <marco.fistler@gmail.com>
2026-06-24 15:19:11 +0200

    adding gitignore

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtCiDemo_FILE C:/dev/qt-ci-demo/build/MinSizeRel/QtCiDemo.exe)
-set(__QT_DEPLOY_TARGET_QtCiDemo_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_QtCiDemo_RUNTIME_DLLS C:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Core.dll)
-set(__QT_DEPLOY_TARGET_UnitTests_FILE C:/dev/qt-ci-demo/build/MinSizeRel/UnitTests.exe)
-set(__QT_DEPLOY_TARGET_UnitTests_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_UnitTests_RUNTIME_DLLS C:/Qt/6.11.1/msvc2022_64/bin/Qt6Test.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Core.dll)
